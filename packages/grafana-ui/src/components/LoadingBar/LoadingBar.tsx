97d40ea26324e448c78cae07780a9ab168af0d95
Torkel Ödegaard <torkel@grafana.com>
2023-02-07 08:40:47 +0100

    PanelChrome: Make loading animation speed the same

--- packages/grafana-ui/src/components/LoadingBar/LoadingBar.tsx
+++ packages/grafana-ui/src/components/LoadingBar/LoadingBar.tsx
@@ -1,48 +1,45 @@
 import { css, keyframes } from '@emotion/css';
-import React from 'react';
-
-import { GrafanaTheme2 } from '@grafana/data';
+import React, { CSSProperties } from 'react';
 
 import { useStyles2 } from '../../themes';
 
 export interface LoadingBarProps {
-  width?: string;
-  height?: string;
+  width: number;
   ariaLabel?: string;
 }
 
-export function LoadingBar({ width, height, ariaLabel = 'Loading bar' }: LoadingBarProps) {
-  const styles = useStyles2(getStyles(width, height));
+const PIXELS_PER_MILLISECOND = 2.1;
+
+export function LoadingBar({ width, ariaLabel = 'Loading bar' }: LoadingBarProps) {
+  const styles = useStyles2(getStyles);
+  const animationSpeed = Math.min(Math.max(Math.round(width * PIXELS_PER_MILLISECOND), 500), 4000);
+  console.log('animationSpeed', animationSpeed);
+  const containerStyles: CSSProperties = {
+    width: '100%',
+    animation: `${styles.animation} ${animationSpeed}ms infinite linear`,
+    willChange: 'transform',
+  };
 
   return (
-    <div className={styles.container}>
+    <div style={containerStyles}>
       <div aria-label={ariaLabel} className={styles.bar} />
     </div>
   );
 }
 
-const getStyles = (width?: string, height?: string) => (_: GrafanaTheme2) => {
-  const barWidth = width ?? '128px';
-  const loadingHeigth = height ?? '2px';
-
-  const loadingAnimation = keyframes({
-    '0%': {
-      transform: 'translateX(0)',
-    },
-    '100%': {
-      transform: `translateX(100%)`,
-    },
-  });
-
+const getStyles = () => {
   return {
-    container: css({
-      width: '100%',
-      animation: `${loadingAnimation} 1s infinite linear`,
-      willChange: 'transform',
+    animation: keyframes({
+      '0%': {
+        transform: 'translateX(-50%)',
+      },
+      '100%': {
+        transform: `translateX(100%)`,
+      },
     }),
     bar: css({
-      width: barWidth,
-      height: loadingHeigth,
+      width: '28%',
+      height: 1,
       background: 'linear-gradient(90deg, rgba(110, 159, 255, 0) 0%, #6E9FFF 80.75%, rgba(110, 159, 255, 0) 100%)',
     }),
   };
